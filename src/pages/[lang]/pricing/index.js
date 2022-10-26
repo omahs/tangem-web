b62b16e11571624a6fc5b4f807f583ae8ad03781
Pavel Sidorov <sidorovpav@ya.ru>
2022-10-26 16:09:07 +0300

    JS-93 fix pricing page

--- src/pages/[lang]/pricing/index.js
+++ src/pages/[lang]/pricing/index.js
@@ -17,6 +17,45 @@ import {SHOPIFY_API_KEY, SHOPIFY_DOMAIN} from "../../../config";
 import ArrowIcon from "../../../../public/svg/faq_arrow.svg";
 import {GiftContext} from "../../../context/gift-context";
 
+const packs = [
+  {
+    id: 'pack3',
+    productId: '6677839577154',
+    title: t('pricing.pack3.title'),
+    description: t('pricing.pack3.description'),
+    image: <picture className={styles.img}>
+      <source srcSet="/img/packs/pack3@1x.avif 1x, /img/packs/pack3@2x.avif 2x" type="image/avif" />
+      <source srcSet="/img/packs/pack3@1x.webp 1x, /img/packs/pack3@2x.webp 2x" type="image/webp" />
+      <img
+        loading='lazy'
+        decoding='async'
+        alt='Pack of 3 Cards'
+        src='/img/packs/pack3@1x.png'
+        srcSet="/img/packs/pack3@2x.png 2x"
+      />
+    </picture>,
+    defaultPrice: '69.90',
+  },
+  {
+    id: 'pack2',
+    productId: '6677836693570',
+    title: t('pricing.pack2.title'),
+    description: t('pricing.pack2.description'),
+    image: <picture className={styles.img}>
+      <source srcSet="/img/packs/pack2@1x.avif 1x, /img/packs/pack2@2x.avif 2x" type="image/avif" />
+      <source srcSet="/img/packs/pack2@1x.webp 1x, /img/packs/pack2@2x.webp 2x" type="image/webp" />
+      <img
+        loading='lazy'
+        decoding='async'
+        alt='Pack of 2 Cards'
+        src='/img/packs/pack2@1x.png'
+        srcSet="/img/packs/pack2@2x.png 2x"
+      />
+    </picture>,
+    defaultPrice: '54.90',
+  }
+];
+
 const LangPricingPage = ({prices}) => {
   const {language} = i18next;
   const resellersLocales = ['ru', 'by'];
@@ -26,45 +65,6 @@ const LangPricingPage = ({prices}) => {
   const useShopify = !resellersLocales.includes(language);
 
   const { isGiftEnabled } = useContext(GiftContext);
-
-  const packs = [
-    {
-      id: 'pack3',
-      productId: '6677839577154',
-      title: t('pricing.pack3.title'),
-      description: t('pricing.pack3.description'),
-      image: <picture className={styles.img}>
-        <source srcSet="/img/packs/pack3@1x.avif 1x, /img/packs/pack3@2x.avif 2x" type="image/avif" />
-        <source srcSet="/img/packs/pack3@1x.webp 1x, /img/packs/pack3@2x.webp 2x" type="image/webp" />
-        <img
-          loading='lazy'
-          decoding='async'
-          alt='Pack of 3 Cards'
-          src='/img/packs/pack3@1x.png'
-          srcSet="/img/packs/pack3@2x.png 2x"
-        />
-      </picture>,
-      defaultPrice: '69.90',
-    },
-    {
-      id: 'pack2',
-      productId: '6677836693570',
-      title: t('pricing.pack2.title'),
-      description: t('pricing.pack2.description'),
-      image: <picture className={styles.img}>
-        <source srcSet="/img/packs/pack2@1x.avif 1x, /img/packs/pack2@2x.avif 2x" type="image/avif" />
-        <source srcSet="/img/packs/pack2@1x.webp 1x, /img/packs/pack2@2x.webp 2x" type="image/webp" />
-        <img
-          loading='lazy'
-          decoding='async'
-          alt='Pack of 2 Cards'
-          src='/img/packs/pack2@1x.png'
-          srcSet="/img/packs/pack2@2x.png 2x"
-        />
-      </picture>,
-      defaultPrice: '54.90',
-    }
-  ];
 
   const [currentPack, setCurrentPack] = useState(packs[0]);
   const [quantity, setQuantity] = useState(1);
@@ -189,7 +189,7 @@ const LangPricingPage = ({prices}) => {
       });
     }
 
-  },[shopifyLoaded, packs]);
+  },[shopifyLoaded]);
 
   useEffect(() => {
     if(!refResellers.current) {
